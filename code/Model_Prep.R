7a69dcfed3bdb8bd1a6753aaa9660590569e4a80
GraceGrant <126471125+GraceGrant@users.noreply.github.com>
2023-06-20 16:59:31 +0200

    More model prep

--- code/Model_Prep.R
+++ code/Model_Prep.R
@@ -1,6 +1,6 @@
 model_prep <- function(result_Large_events){
 
-    filter <- result_Large_events %>% filter(year(date) >= 2003) %>%
+    filter <- result_Large_events %>% filter(lubridate::year(date) >= 2003) %>%
         select(-winner_rank_points, -loser_rank_points, -winner_hand, -loser_hand, -winner_entry, -loser_entry,
                -winner_seed, -loser_seed, -tourney_date) %>% filter(!is.na(minutes)) %>% na.omit()
 
